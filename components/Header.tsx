aa2b097d051d8a483d2327284ceb4e14fcfe6c7b
Rodolfo Belo <rodolfo.silva.belo@gmail.com>
2023-01-08 21:11:22 -0300

    fix header

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,7 +36,6 @@ export function Header() {
         </div>
       </div>
 
-
       {/* center */}
       <div className="flex justify-center flex-grow">
         <div className="flex space-x-6 md:space-x-2">
@@ -52,11 +51,11 @@ export function Header() {
       <div className="flex items-center sm:space-x-2 justify-end">
         {/* Profile Pic */}
 
-        <p className="whitespace-nowrap font-semibold pr-3">Rodolfo Belo</p> 
-        <ViewGridIcon className="icon"/>
-        <ChatIcon className="icon"/>
-        <BellIcon className="icon"/>
-        <ChevronDownIcon className="icon"/>       
+        <p className="whitespace-nowrap font-semibold pr-3">Rodolfo Belo</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
       </div>
 
     </div>
